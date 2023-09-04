a224eb573cf337d6bfd461f29b13ced8271b2085
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-09-04 20:45:13 +0200

    Calc

--- zadanie_4.py
+++ zadanie_4.py
@@ -33,3 +33,43 @@ def oblicz_iloczyn(liczby):
     for liczba in liczby:
         iloczyn = iloczyn * liczba
     return iloczyn
+
+def kalkulator():
+    while True:
+        print("\nPodaj działanie posługując się odpowiednią liczbą:")
+        print("1. Dodawanie")
+        print("2. Odejmowanie")
+        print("3. Mnożenie")
+        print("4. Dzielenie")
+        print("5. Zakończ program")
+
+        wybor = input("Twój wybór: ")
+
+        if wybor == '1':
+            liczby = wprowadz_liczby()
+            wynik = oblicz_sume(liczby)
+            print("Wynik dodawania:", wynik)
+        elif wybor == '2':
+            x = wprowadz_liczbe()
+            y = wprowadz_liczbe()
+            wynik  = x - y
+            print("Wynik odejmowania:", wynik)
+        elif wybor == '3':
+            liczby = wprowadz_liczby()
+            wynik = oblicz_iloczyn(liczby)
+            print("Wynik mnożenia:", wynik)
+        elif wybor == '4':
+            x = wprowadz_liczbe()
+            y = wprowadz_liczbe()
+            if y != 0:
+                wynik = x/y
+                print("Wynik dzielenia:", wynik)
+            else:
+                print("Nie można dizelić przez zero.")
+        elif wybor == '5':
+            print("Program zakończony.")
+            break
+        else:
+            print("Nieprawidłowy wybór. Spróbuj ponownie.")
+
+kalkulator()
