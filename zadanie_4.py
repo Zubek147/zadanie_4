6b59814d0f715cb111be9d243ca8d8871637cbae
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-09-04 20:38:39 +0200

    Add def enter number

--- zadanie_4.py
+++ zadanie_4.py
@@ -1,2 +1,9 @@
 #Zadanie z 4 modułu - kalkulator
 
+def wprowadz_liczbe():
+    while True:
+        try:
+            liczba =float(input("Wprowadź liczbę: "))
+            return liczba
+        except ValueError:
+            print("To nie jest prawidłowa liczba. Spróbuj ponownie.")
