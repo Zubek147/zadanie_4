48c50e1b409a207e73fc91fcc0b87bb01a794662
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-09-14 16:34:04 +0200

    Add definition of multi

--- zadanie_4.py
+++ zadanie_4.py
@@ -31,13 +31,29 @@ def dodawanie():
     logging.info("Wywołuję funkcję dodawanie.")
     liczby = wprowadz_liczby()
     wynik = sum(liczby)
+    logging.info(f"Wynik dodawania to: {wynik}")
     print(f"Wynik dodawania to: {wynik}")
-    logging.info((f"Wynik dodawania to: {wynik}"))
-
+    
 def odejmowanie():
     logging.info("Wywołuję funkcję odejmowanie.")
     x = wprowadz_liczbe("Podaj pierwszą liczbę: ")
     y = wprowadz_liczbe("Podaj drugą liczbę: ")
     wynik = x - y
+    logging.info(f"Wynik odejmowania to: {wynik}")
     print(f"Wynik odejmowania to: {wynik}")
-    logging.info((f"Wynik odejmowania to: {wynik}"))
+    
+def mnozenie():
+    logging.info("Wywołuję funkcję mnożenia")
+    liczby = wprowadz_liczby()
+
+    if len(liczby) <2:
+        logging.error("Potrzebujesz co najmniej dwóch liczb.")
+        print("Potrzebujesz co najmniej dwóch liczb.")
+    else:
+        wynik = 1
+
+        for liczba in liczby:
+            wynik *= liczba
+    logging.info(f"Wynik mnożenia to: {wynik}")        
+    print(f"Wynik mnożenia to: {wynik}")
+
