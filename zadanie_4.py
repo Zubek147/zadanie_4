16f94db98c923bd2b2627686025545c4e46643f2
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2024-01-04 11:30:10 +0100

    Finall version after changes

--- zadanie_4.py
+++ zadanie_4.py
@@ -1,106 +1,60 @@
 #Zadanie z 4 modułu - kalkulator
 import logging
-log_directory = "C:\\Kodilla\\kurs_python\\Modul 4\\Zad_4"
+
+log_directory = "C:\\Kodilla\\kurs_python\\Modul 4\\Zad_4\\Poprawka"
 
 log_file = f"{log_directory}/kalkulator.log"
 
-logging.basicConfig(filename='kalkulator.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger()
 
-def wprowadz_liczbe():
-    while True:
-        try:
-            liczba =float(input("Wprowadź liczbę: "))
-            return liczba
-        except ValueError:
-            logging.error("To nie jest prawidłowa liczba. Spróbuj ponownie.")
-            print("To nie jest prawidłowa liczba. Spróbuj ponownie.")
-
-def wprowadz_liczby():
-    liczby = []
-    while True:
-        try:
-            wprowadzona_wartosc = input("Wprowadź liczbę lub wpisz 'koniec', aby obliczyć wynik: ")
-            if wprowadzona_wartosc.lower() == 'koniec':
-                if len(liczby) < 2:
-                    logging.error("Potrzebujesz co najmniej dwóch liczb.")
-                    print("Potrzebujesz co najmniej dwóch liczb.")
-                else:
-                    return liczby
-            else:
-                liczba = float(wprowadzona_wartosc)
-                liczby.append(liczba)
-        except ValueError:
-            logging.error("To nie jest prawidłowa liczba. Spróbuj ponownie.")
-            print("To nie jest prawidłowa liczba. Spróbuj ponownie.")
-
-def dodawanie():
-    logging.info("Wywołuję funkcję dodawanie.")
-    print("Wywołuję funkcję dodawanie.")
-    liczby = wprowadz_liczby()
-    wynik = sum(liczby)
-    liczby_str = ', '.join(map(str, liczby))
-    logging.info(f"Dodane liczby to: {liczby_str}, wynik dodawania to: {wynik}")
+def dodawanie(*args):
+    wynik = sum(args)
+    liczby_str = ', '.join(map(str, args))
+    logger.info(f"Dodane liczby to: {liczby_str}, wynik dodawania to: {wynik}")
     print(f"Dodane liczby to: {liczby_str}, wynik dodawania to: {wynik}")
-    
-def odejmowanie():
-    logging.info("Wywołuję funkcję odejmowanie.")
-    print("Wywołuję funkcję odejmowanie.")
-    x = wprowadz_liczbe()
-    y = wprowadz_liczbe()
-    wynik = x - y
-    logging.info(f"Pierwsza liczba to: {x}, liczba odejmowana to: {y}, wynik odejmowania to: {wynik}")
-    print(f"Pierwsza liczba to: {x}, liczba odejmowana to: {y}, wynik odejmowania to: {wynik}")
-    
-def mnozenie():
-    logging.info("Wywołuję funkcję mnożenia")
-    print("Wywołuję funkcję mnożenia")
-    liczby = wprowadz_liczby()
 
-    if len(liczby) <2:
-        logging.error("Potrzebujesz co najmniej dwóch liczb.")
+def odejmowanie(x, y):
+    wynik = x - y
+    logger.info(f"Pierwsza liczba to: {x}, liczba odejmowana to: {y}, wynik odejmowania to: {wynik}")
+    print(f"Pierwsza liczba to: {x}, liczba odejmowana to: {y}, wynik odejmowania to: {wynik}")
+
+def mnozenie(*args):
+    if len(args) < 2:
+        logger.error("Potrzebujesz co najmniej dwóch liczb.")
         print("Potrzebujesz co najmniej dwóch liczb.")
     else:
         wynik = 1
-
-        for liczba in liczby:
+        liczby_str = ', '.join(map(str, args))
+        for liczba in args:
             wynik *= liczba
-            liczby_str = ', '.join(map(str, liczby))
-    logging.info(f"Liczby które zostały przemnożone to: {liczby_str}, wynik mnożenia to: {wynik}")        
-    print(f"Liczby które zostały przemnożone to: {liczby_str}, wynik mnożenia to: {wynik}")
+        logger.info(f"Liczby które zostały przemnożone to: {liczby_str}, wynik mnożenia to: {wynik}")
+        print(f"Liczby które zostały przemnożone to: {liczby_str}, wynik mnożenia to: {wynik}")
 
-def dzielenie():
-    logging.info("Wywołuję funkcję dzielenia")
-    print("Wywołuję funkcję dzielenia")
-    x = wprowadz_liczbe()
-
-    while True:
-        y = wprowadz_liczbe()
-        try:
-            wynik = x / y
-            if y == 0:
-                raise ZeroDivisionError("Nie można dzielić przez zero, podaj poprawną wartość")
-            logging.info(f"Liczba Dzielna to: {x}, dzielnik to: {y}, wynik dzielenia to: {wynik}")
-            print(f"Liczba Dzielna to: {x}, dzielnik to: {y}, wynik dzielenia to: {wynik}")
-            break
-        except (ZeroDivisionError, Exception):
-            logging.error("Nie można dzielić przez zero, podaj poprawną wartość")
-            print("Nie można dzielić przez zero, podaj poprawną wartość")
-
+def dzielenie(x, y):
+    try:
+        wynik = x / y
+        if y == 0:
+            raise ZeroDivisionError("Nie można dzielić przez zero, podaj poprawną wartość")
+        logger.info(f"Liczba Dzielna to: {x}, dzielnik to: {y}, wynik dzielenia to: {wynik}")
+        print(f"Liczba Dzielna to: {x}, dzielnik to: {y}, wynik dzielenia to: {wynik}")
+    except (ZeroDivisionError, Exception):
+        logger.error("Nie można dzielić przez zero, podaj poprawną wartość")
+        print("Nie można dzielić przez zero, podaj poprawną wartość")
 
 def zakoncz_program():
-    logging.info("Wybrano zakończenie programu")
+    logger.info("Wybrano zakończenie programu")
     print("Wybrano zakończenie programu")
     exit()
 
 def kalkulator():
     operacje = {
-        '1' : dodawanie,
-        '2' : odejmowanie,
-        '3' : mnozenie,
-        '4' : dzielenie,
-        '5' : zakoncz_program
-    }   
+        '1': dodawanie,
+        '2': odejmowanie,
+        '3': mnozenie,
+        '4': dzielenie,
+        '5': zakoncz_program
+    }
 
     while True:
         print("\nPodaj działanie posługując się odpowiednią liczbą:")
@@ -112,9 +66,30 @@ def kalkulator():
 
         wybor = input("Twój wybór: ")
 
-        operacja = operacje.get(wybor)
-        if operacja:
-            operacja()
+        if wybor == '5':
+            operacje[wybor]()
+        elif wybor in {'1', '2', '3', '4'}:
+            if wybor in {'1', '3'}:
+                liczby = []
+                while True:
+                    wprowadzona_wartosc = input("Wprowadź liczbę lub wpisz 'koniec', aby obliczyć wynik: ")
+                    if wprowadzona_wartosc.lower() == 'koniec':
+                        if len(liczby) < 2:
+                            logger.error("Potrzebujesz co najmniej dwóch liczb.")
+                            print("Potrzebujesz co najmniej dwóch liczb.")
+                        else:
+                            operacje.get(wybor)(*liczby)
+                            break
+                    elif wprowadzona_wartosc.isdigit():
+                        liczba = float(wprowadzona_wartosc)
+                        liczby.append(liczba)
+                    else:
+                        logger.error("To nie jest prawidłowa liczba. Spróbuj ponownie.")
+                        print("To nie jest prawidłowa liczba. Spróbuj ponownie.")
+            else:
+                x = float(input("Wprowadź pierwszą liczbę: "))
+                y = float(input("Wprowadź drugą liczbę: "))
+                operacje.get(wybor)(x, y)
         else:
             print("Nieprawidłowy wybór!")
 
